cf5ef939dde5c8b5517a0ab2f9fd6483feca84f2
Nicky Wisenov <wisneov@gmail.com>
2018-11-30 12:37:03 +0000

    change some

--- config/db.php
+++ config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-	'dsn' => 'mysql:host=localhost;dbname=edenwoolf_dev',
+	'dsn' => 'mysql:host=127.0.0.1;dbname=edenwoolf_dev',
 	'username' => 'root',
-	'password' => 'root',
+	'password' => '',
     'charset' => 'utf8',
 ];
